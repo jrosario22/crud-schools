bca567498e80b8705c343f81e85fdb40b09401ac
Jeson Rosario <jesonrosario22@gmail.com>
2019-06-14 11:27:49 -0400

    Added axios function

--- src/campusListing.js
+++ src/campusListing.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import pool from './campusInfo';
+import Axios from 'axios';
 
 class CampusListing extends React.Component{
+    // constructor(props){
+    //     super(props){
+
+    //     }
+    // }
+    something = () =>  {
+        Axios.get('http://localhost:3000/campus')
+        .then(()=>{console.log("did get request")})
+        .catch(err => console.log(err));
+    }
+
     render(){
-        return(
+        return( 
             <div>
                 <h1>All Campuses</h1>
-                <button>Add Campus</button>
+                {/* <p>{pool}</p> */}
+                <button onClick={this.something}>Add Campus</button>
             </div>
         )
     }
